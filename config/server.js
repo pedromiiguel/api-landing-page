0a78f2aa91091c36fc7252ff9a1d271e5a7ce858
Pedro Miguel <pedromrap@gmail.com>
2021-11-03 09:10:48 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed61452f663f39c0c101f148a4bbd66c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dde47298-acc7-4083-9342-40eeeda2ea94'
+};
